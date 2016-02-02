661df3c1cca1ec40b30c92b929d735b2de8ab013
mimoto.t <mimoto@w-tokyodo.com>
2016-02-02 16:26:16 +0900

    add .po.mo file. load textdomain.

--- CoreRepository.php
+++ CoreRepository.php
@@ -5,3 +5,13 @@ interface CoreRepository {
 	public static function init( $name, $args );
 	public static function instance( $name );
 }
+
+/**
+ * Load textdomain.
+ */
+if ( defined( 'ABSPATH' ) ) {
+	load_textdomain(
+		'mimosafa-wp-core-repository',
+		__DIR__ . '/languages/wp-core-repository-' . get_locale() . '.mo'
+	);
+}
